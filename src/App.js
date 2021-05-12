ed5416091dcd3453b650cd9f18de59be2d6e702b
edufletch <mail.levifletcher@gmail.com>
2021-05-12 15:40:33 -0700

    edit text test

--- src/App.js
+++ src/App.js
@@ -14,7 +14,7 @@ function App() {
           href='https://reactjs.org'
           target='_blank'
           rel='noopener noreferrer'>
-          Learn React over multiple years
+          Learn React slowly
         </a>
       </header>
     </div>
